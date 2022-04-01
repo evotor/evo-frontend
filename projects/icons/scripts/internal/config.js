07e7892f83ec1c3a9c97c9f127c22118354e5d43
andreymatveev <andrey.g.matveev@gmail.com>
2022-04-01 13:32:31 +0700

    chore(icons): rename generated dir to dist

--- projects/icons/scripts/internal/config.js
+++ projects/icons/scripts/internal/config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const PROJECT_ROOT_PATH = path.join(__dirname, '../../');
 const SRC_PATH = path.join(PROJECT_ROOT_PATH, 'src/svg/monochrome-icons');
-const DIST_PATH = path.join(PROJECT_ROOT_PATH, 'generated');
+const DIST_PATH = path.join(PROJECT_ROOT_PATH, 'dist');
 
 module.exports = {
     PROJECT_ROOT_PATH,
