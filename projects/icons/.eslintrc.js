fece54e7d5b2a6f8cf2b628b9fb46f782de623f0
andreymatveev <andrey.g.matveev@gmail.com>
2023-07-17 14:35:44 +0700

    chore(icons): fixes

--- projects/icons/.eslintrc.js
+++ projects/icons/.eslintrc.js
@@ -1,18 +1,3 @@
 module.exports = {
     extends: ['@evo/eslint-config-ng', 'prettier'],
-    overrides: [
-        {
-            files: ['*.ts'],
-            parserOptions: {
-                project: ['./tsconfig.lib.json'],
-                ecmaVersion: 2020,
-                sourceType: 'module',
-            },
-        },
-    ],
-    env: {
-        es6: true,
-        browser: true,
-        node: true,
-    },
 };
